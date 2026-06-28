10e7d7e86f310be2034e4f3a3f9395167fa2105e
Aditya-Samarth <Aditya.Samarth@protonmail.com>
2026-06-28 20:32:42 +0530

    just adding more things in code

--- user_scanner.py
+++ user_scanner.py
@@ -3,6 +3,9 @@ import argparse
 import concurrent.futures
 import threading
 
+urls = {"url1": "https://thehackernews.com/"}
 username = input("Enter your username you want to find: ")
 
+respond = requests.get(urls["url1"])
 
+print(respond)
